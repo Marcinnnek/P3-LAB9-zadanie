afdadc8b72336226342a1df7e613bb215cab96de
Marcin Zuziak <porda1995@gmail.com>
2021-02-02 18:58:50 +0100

    test

---
+++ P3 LAB9 zadanie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P3_LAB9_zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
